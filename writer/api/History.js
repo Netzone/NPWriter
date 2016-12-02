650046dc3e639a483f34c539a95636b9f5ab80a6
Andreas Kihlberg <andreas.kihlberg@infomaker.se>
2016-12-02 10:15:33 +0100

    Removed console logging for history

--- writer/api/History.js
+++ writer/api/History.js
@@ -205,13 +205,12 @@ class History {
         let id = this.api.newsItem.getIdForArticle()
 
 
-        var _lsTotal = 0, _xLen, _x;
-
-        for (_x in localStorage) {
-            _xLen = ((localStorage[_x].length + _x.length) * 2);
-            _lsTotal += _xLen;
-            console.info(_x.substr(0, 50) + " using " + (_xLen / 1024).toFixed(2) + " KB")
-        }
+        // var _lsTotal = 0, _xLen, _x;
+        // for (_x in localStorage) {
+        //     _xLen = ((localStorage[_x].length + _x.length) * 2);
+        //     _lsTotal += _xLen;
+            // console.info(_x.substr(0, 50) + " using " + (_xLen / 1024).toFixed(2) + " KB")
+        // }
 
         let doc = this.get(id)
 
