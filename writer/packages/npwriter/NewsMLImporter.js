d354a7676f5a26dc5963a1900b4ecb71801ae3cd
Andreas Kihlberg <andreas.kihlberg@infomaker.se>
2016-12-06 10:15:52 +0100

    Fix converter for teaser

--- writer/packages/npwriter/NewsMLImporter.js
+++ writer/packages/npwriter/NewsMLImporter.js
@@ -1,7 +1,6 @@
-import { XMLImporter } from 'substance'
+import {XMLImporter} from 'substance'
 import isArray from 'lodash/isArray'
 import find from 'lodash/find'
-
 /**
  * NewsMLImporter Importer
  */
@@ -13,10 +12,10 @@ class NewsMLImporter extends XMLImporter {
      * @returns {*}
      */
     getFirstElementWithTypeElement(newsItemEl) {
-        if(isArray(newsItemEl)) {
-            return find(newsItemEl, function(el) {
+        if (isArray(newsItemEl)) {
+            return find(newsItemEl, function (el) {
                 try {
-                    if( el.nodeType === 'element') {
+                    if (el.nodeType === 'element') {
                         return true;
                     }
                 } catch (e) {
@@ -29,24 +28,39 @@ class NewsMLImporter extends XMLImporter {
     }
 
     convertDocument(newsItemEl) {
-        var newsItemElement = this.getFirstElementWithTypeElement(newsItemEl);
-        var groups = newsItemElement.findAll('idf > group[type="body"]');
-        var headerGroup = newsItemElement.find('idf > group[type="header"]');
+        const newsItemElement = this.getFirstElementWithTypeElement(newsItemEl);
+        const groups = newsItemElement.findAll('idf > group[type="body"]');
+        const headerGroup = newsItemElement.find('idf > group[type="header"]');
 
         // Convert headergroup if it exist
-        if(headerGroup) {
+        if (headerGroup) {
             this.convertElement(headerGroup);
         }
 
         // Convert body groups
-        groups.forEach(function(groupEl) {
-            var teaserEl = newsItemElement.findAll('contentMeta > metadata > object[type="x-im/teaser"]');
+        groups.forEach((groupEl) => {
+            const teaserEl = newsItemElement.findAll('contentMeta > metadata > object[type="x-im/teaser"]');
             if (teaserEl && teaserEl.length === 1) {
-                groupEl.append(teaserEl[0]);
+
+                const teaser = teaserEl[0]
+                const teaserPosition = this.context.api.getConfigValue('se.infomaker.ximteaser', 'teaserPosition', 'bottom')
+                // Teaser at top
+                if (teaserPosition === 'top') {
+                    const parent = groupEl.el
+                    const firstChild = groupEl.children[0]
+                    parent.insertBefore(teaser.el, firstChild.el)
+                } else {
+
+                    // Teaser at the bottom
+                    groupEl.append(teaser);
+                }
             }
+
             this.convertElement(groupEl);
-        }.bind(this));
+        })
+
     }
 }
+
 
 export default NewsMLImporter
