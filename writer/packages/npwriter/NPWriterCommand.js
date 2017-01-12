802d750bd1a701bbfd02ecaf72f96b98279adaf2
Andreas Kihlberg <andreas.kihlberg@infomaker.se>
2017-01-12 14:53:47 +0100

    Added check for commands if they should be visible when IsolatedNodes is selected

--- writer/packages/npwriter/NPWriterCommand.js
+++ writer/packages/npwriter/NPWriterCommand.js
@@ -3,8 +3,20 @@ import {Command} from 'substance'
 class NPWriterCommand extends Command {
 
     getCommandState(params) {
+
+        let isDisabled = false
+
+        // Check if this is a nodeSelection, in that case the contentMenu tools should be disabled
+        if(params.surface && params.selection.isNodeSelection) {
+            isDisabled = true
+        }
+
+        if(params.surface && params.surface.name === 'body') {
+            isDisabled = true
+        }
+
         return {
-            disabled: params.surface && params.surface.name === 'body' ? false : true
+            disabled: isDisabled
         }
     }
 
