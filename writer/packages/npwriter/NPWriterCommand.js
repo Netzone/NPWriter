5fde8334de5e8456fe2af5b02ffc901fad8dd29b
Andreas Kihlberg <andreas.kihlberg@infomaker.se>
2017-01-13 10:26:58 +0100

    Fixed issue where content tools where visible when is shouldn’t

--- writer/packages/npwriter/NPWriterCommand.js
+++ writer/packages/npwriter/NPWriterCommand.js
@@ -4,15 +4,13 @@ class NPWriterCommand extends Command {
 
     getCommandState(params) {
 
-        let isDisabled = false
+        let isDisabled = true
 
         // Check if this is a nodeSelection, in that case the contentMenu tools should be disabled
-        if(params.surface && params.selection.isNodeSelection) {
+        if(params.surface && params.selection.isNodeSelection()) {
             isDisabled = true
-        }
-
-        if(params.surface && params.surface.name === 'body') {
-            isDisabled = true
+        } else if(params.surface && params.surface.name === 'body') {
+            isDisabled = false
         }
 
         return {
