14cdd5729f30c1549a00c8bd5d14bdf888aff052
Danne Lundqvist <danne@dotvoid.se>
2017-02-21 16:04:50 +0100

    Make sure about information is shown in different languages and not mere language labels.

--- writer/packages/about/AboutPackage.js
+++ writer/packages/about/AboutPackage.js
@@ -5,6 +5,8 @@ import AboutComponent from './AboutComponent'
 export default {
     name: 'about',
     configure: function(config) {
+        const aboutVersion = "NP Writer 3.0 pre-beta"
+
         config.addPopover(
             'npwriterabout',
             {
@@ -19,17 +21,19 @@ export default {
         )
 
         config.addLabel('about-header', {
-            en: "NP Writer 3.0 alpha",
-            sv: "NP Writer 3.0 alpha"
+            en: aboutVersion,
+            sv: aboutVersion,
+            nl: aboutVersion,
+            fi: aboutVersion,
+            no: aboutVersion,
+            dk: aboutVersion
         })
 
-        config.addLabel('about-description', {
-            en: "Newspilot Writer is brought to you by Infomaker Scandinavia AB and was first released in early 2016.",
+        config.addLabel('Newspilot Writer is brought to you by Infomaker Scandinavia AB and was first released in early 2016.', {
             sv: "Newspilot Writer skapad av Infomaker Scandinavia AB och släpptes första gången våren 2016."
         })
 
-        config.addLabel('about-credits', {
-            en: "Core team and credits",
+        config.addLabel('Core team and credits', {
             sv: "Team och credits",
         })
     }
